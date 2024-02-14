71d361c945010d0407e976cbe282380c4c161180
Erick Namukolo <erickmndev@gmail.com>
2024-02-14 21:28:08 +0200

    made the star clickable

--- src/components/star-rating.tsx
+++ src/components/star-rating.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const StarRating: React.FC<{ maxRating?: number }> = ({ maxRating = 10 }) => {
+  const [rating, setRating] = useState(0);
+
+  function updateRating(newRating: number) {
+    setRating(newRating);
+  }
   const containerStyle = {
     display: 'flex',
     alignItems: 'center',
@@ -9,7 +14,6 @@ const StarRating: React.FC<{ maxRating?: number }> = ({ maxRating = 10 }) => {
 
   const starContainerStyle = {
     display: 'flex',
-    gap: '4px',
   };
 
   const textStyle = {
@@ -20,11 +24,33 @@ const StarRating: React.FC<{ maxRating?: number }> = ({ maxRating = 10 }) => {
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span>{i + 1}</span>
+          <Star key={i} click={() => updateRating(i + 1)} />
         ))}
       </div>
-      <p style={textStyle}>1</p>
+      <p style={textStyle}>{rating || ''}</p>
     </div>
+  );
+};
+
+const starStyle = {
+  width: '48px',
+  height: '48px',
+  display: 'block',
+  cursor: 'pointer',
+};
+
+const Star: React.FC<{ click: () => void }> = ({ click }) => {
+  return (
+    <span style={starStyle} onClick={click}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 20 20'
+        fill='#000'
+        stroke='#000'
+      >
+        <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+      </svg>
+    </span>
   );
 };
 
