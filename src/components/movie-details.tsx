3ff5e2975475c9ecee99aa5f694c264ba631ca47
Erick Namukolo <erickmndev@gmail.com>
2024-02-21 20:37:25 +0200

    adde func to fetch detailed movie details

--- src/components/movie-details.tsx
+++ src/components/movie-details.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import Movie from '../models/movie';
+import apiKey from '../constants/keys';
 
 export default function MovieDetails({
   selectedMovie,
@@ -7,9 +9,32 @@ export default function MovieDetails({
   selectedMovie: Movie;
   onClose: () => void;
 }) {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState<Movie | null>(null);
+  useEffect(() => {
+    fetchMovieDetails();
+  }, []);
+
+  async function fetchMovieDetails() {
+    try {
+      const res: Response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&i=${selectedMovie.imdbID}&plot=full`
+      );
+
+      const data = await res.json();
+      setMovie(data as Movie);
+    } catch (e) {
+      alert(e);
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
-    <div className='details' onClick={onClose}>
-      {selectedMovie.imdbID}
+    <div className='details'>
+      <button className='btn-back' onClick={onClose}>
+        &larr;
+      </button>
+      {movie?.imdbID}
     </div>
   );
 }
