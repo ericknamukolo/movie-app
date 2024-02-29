f9d518b905704306ff74e76a2bbe383c53e9faf8
Erick Namukolo <erickmndev@gmail.com>
2024-02-29 14:43:40 +0200

    added local staorage

--- src/App.tsx
+++ src/App.tsx
@@ -6,68 +6,19 @@ import Summary from './components/summary';
 import AppContainer from './layouts/app-container';
 import Box from './layouts/box';
 import MovieList from './components/movie-list';
-import StarRating from './components/star-rating';
 import apiKey from './constants/keys';
 import Loader from './components/loader';
 import MovieDetails from './components/movie-details';
-
-const tempMovieData: Movie[] = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt0133093',
-    Title: 'The Matrix',
-    Year: '1999',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt6751668',
-    Title: 'Parasite',
-    Year: '2019',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
-  },
-];
-
-const tempWatchedData: Movie[] = [
-  // {
-  //   imdbID: 'tt1375666',
-  //   Title: 'Inception',
-  //   Year: '2010',
-  //   Poster:
-  //     'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-  //   runtime: 148,
-  //   imdbRating: 8.8,
-  //   userRating: 10,
-  // },
-  // {
-  //   imdbID: 'tt0088763',
-  //   Title: 'Back to the Future',
-  //   Year: '1985',
-  //   Poster:
-  //     'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-  //   runtime: 116,
-  //   imdbRating: 8.5,
-  //   userRating: 9,
-  // },
-];
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [query, setQuery] = useState('batman');
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-  const [watched, setWatched] = useState<Movie[]>([]);
-
-  useEffect(() => {
-    fetchMovies(query);
-  }, [query]);
+  const [watched, setWatched] = useState<Movie[]>(function () {
+    const watchedMovies: Movie[] = JSON.parse(localStorage.getItem('watched')!);
+    return watchedMovies;
+  });
 
   async function fetchMovies(query: string) {
     try {
@@ -105,6 +56,7 @@ export default function App() {
         Title: movie.Title,
         Poster: movie.Poster,
         Year: movie.Year,
+        imdbRating: movie.imdbRating,
         runtime: Number(movie.Runtime?.split(' ').at(0)),
       };
       setWatched((prev) => {
@@ -116,6 +68,14 @@ export default function App() {
       });
     }
   }
+
+  useEffect(() => {
+    fetchMovies(query);
+  }, [query]);
+
+  useEffect(() => {
+    localStorage.setItem('watched', JSON.stringify([...watched]));
+  }, [watched]);
 
   return (
     <>
