54b2e2433ae91c54fb7416b1516b28e5f9b75c3c
Erick Namukolo <erickmndev@gmail.com>
2024-03-03 17:20:28 +0200

    fixed issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import NavBar from './components/nav-bar';
 import Movie from './models/movie';
 import WatchedMovieList from './components/watched-movie-list';
